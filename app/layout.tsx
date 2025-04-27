d5162721f50a10bc58de0fb196d448f13ee01904
NOVSivakumarS23 <73793237+NOVSivakumarS23@users.noreply.github.com>
2025-04-27 06:23:12 -0700

    jslkd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,22 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import { NavbarDemo } from "@/components/parts/navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: "MXR",
+  description: "Milpitas High School's official VEX, FTC, and FRC robotics club",
 };
 
 export default function RootLayout({
@@ -12,7 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+          <div className="relative w-full">
+            <NavbarDemo />
+            {children}
+          </div>
+      </body>
     </html>
   );
 }
--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -9,10 +9,10 @@ import Contact from "@/components/parts/contact";
 import FadeInOnScroll from "@/components/tools/fade-in-on-scroll";
 import BlogCard from "@/components/parts/blog-cards";
 
-import "./css/navbar.css";
-import "./css/main.css";
-import "./css/blog-card.css";
-import "./css/endbar.css";
+import "../css/navbar.css";
+import "../css/main.css";
+import "../css/blog-card.css";
+import "../css/endbar.css";
 
 import Endbar from "@/components/parts/endbar";
 import SponsorUs from "@/components/parts/sponsor-us";
@@ -64,9 +64,6 @@ export default function Home() {
           <img src="images/blog/teamPic2.webp"></img>
         </div>
       </div>
-      <div className='c'>
-      <TypewriterEffect words={[{text:"Check"},{text:"out"},{text:"our"},{text:"blog"},{text:"links!😊"}]} className="h2" />
-      </div> 
       <BlogCard></BlogCard>
       <SponsorUs></SponsorUs>
 
